4044afaf3cfed1feae0e9fabb1d75a9d6029ba3c
jd03249 <jd03249@naver.com>
2020-06-24 20:13:12 +0900

    Styles

--- src/components/Customer.js
+++ src/components/Customer.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
 
 class Customer extends React.Component{
     render(){
